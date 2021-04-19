904868ed9fa76aa5d456c036ab6c02ac7d9721c5
sir-meee <sammymbevi@gmail.com>
2021-04-19 16:07:47 +0300

    movie details

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .requests import get_movies
+from .requests import get_movies, get_movie
 
 # Views
 @app.route('/')
@@ -15,11 +15,22 @@ def index():
     title = 'Home - The best Movie Review Website Online'
     return render_template('index.html', title=title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+# @app.route('/movie/<int:movie_id>')
+# def movie(movie_id):
+
+#     '''
+#     View movie page function that returns the movie details page and its data
+#     '''
+#     title = movie_id
+#     return render_template('movie.html',id = movie_id, title = movie_id)
+
+@app.route('/movie/<int:id>')
+def movie(id):
 
     '''
     View movie page function that returns the movie details page and its data
     '''
-    title = movie_id
-    return render_template('movie.html',id = movie_id, title = movie_id)
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
