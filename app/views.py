f95beb8256927689648612cbf9385ec68bbb1554
sir-meee <sammymbevi@gmail.com>
2021-04-19 14:18:20 +0300

    adding categories

--- app/views.py
+++ app/views.py
@@ -10,9 +10,10 @@ def index():
     View root page function that returns the index page and its data
     '''
     popular_movies = get_movies('popular')
-    print(popular_movies)
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     title = 'Home - The best Movie Review Website Online'
-    return render_template('index.html', title=title, popular = popular_movies)
+    return render_template('index.html', title=title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
