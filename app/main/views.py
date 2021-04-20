087c3fe2c6f152dc0dd205c79ee79a661babf97d
sir-meee <sammymbevi@gmail.com>
2021-04-20 17:40:48 +0300

    Creating a Profile page

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..requests import get_movies, get_movie,search_movie
-from ..models import Review
+from ..models import Review, User
 from .forms import ReviewForm
 from flask_login import login_required
 
@@ -72,3 +72,12 @@ def new_review(id):
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
