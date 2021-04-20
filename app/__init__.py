f4f38f161a2a45def9c332ae8ea84064892ae6e4
sir-meee <sammymbevi@gmail.com>
2021-04-20 08:32:38 +0300

    Registering a Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_bootstrap import Bootstrap
 bootstrap = Bootstrap()
 
 def create_app(config_name):
-    app = Flask(__name__,)
+    app = Flask(__name__)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
@@ -13,5 +13,9 @@ def create_app(config_name):
 
     # Initializing Flask Extensions
     bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
