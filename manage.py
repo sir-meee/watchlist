330d188840c213f7b546e4f1bf50664b5796fd0f
sir-meee <sammymbevi@gmail.com>
2021-04-20 14:40:19 +0300

    role model

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from app import create_app, db
-from app.models import User
+from app.models import User, Role
 from flask_script import Manager,Shell,Server
 
 # Creating app instance
@@ -18,7 +18,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User, Role = Role )
 
 if __name__ == '__main__':
     manager.run()
