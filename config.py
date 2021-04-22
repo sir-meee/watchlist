7c8120c1a4b071fd762884e2ae4d3412ec835456
sir-meee <sammymbevi@gmail.com>
2021-04-22 17:30:28 +0300

    Sending Welcome Email

--- config.py
+++ config.py
@@ -15,10 +15,6 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-    @staticmethod
-    def init_app(app):
-        pass
-
 
 class ProdConfig(Config):
     pass
