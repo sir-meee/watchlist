ac5df67fe79b02d04b857f27b604370e209495ad
sir-meee <sammymbevi@gmail.com>
2021-04-22 16:20:53 +0300

    Sending Welcome Email

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,8 @@ from . import auth
 from ..models import User
 from .forms import RegistrationForm, LoginForm
 from .. import db
+from ..email import mail_message
+
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -26,6 +28,9 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+        
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
