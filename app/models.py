664a70a92d2cdb8fffa7635e91b961554961b0c1
sir-meee <sammymbevi@gmail.com>
2021-04-22 15:42:05 +0300

    error fixes

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ from . import login_manager
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
@@ -17,7 +17,7 @@ class User(db.Model):
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    password_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User {self.username}'
@@ -28,11 +28,11 @@ class User(db.Model):
 
     @password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
 
     def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+        return check_password_hash(self.password_hash,password)
 
 
 class Movie:
