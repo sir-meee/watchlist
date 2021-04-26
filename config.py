f06eb31969e6abb93730a2e839a025108776f706
sir-meee <sammymbevi@gmail.com>
2021-04-26 16:34:51 +0300

    database url

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class TestConfig(Config):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL?sslmode=require").replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace('postgres://', 'postgresql://')
 
 
 class DevConfig(Config):
