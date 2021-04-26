308b6b73d8f8f7e73d5f25c1344dcc273dd2e44b
sir-meee <sammymbevi@gmail.com>
2021-04-26 17:28:50 +0300

    add command

---
+++ commands.py
@@ -0,0 +1,10 @@
+import click
+from flask.cli import with_appcontext
+
+from app import db
+from app.models import User, Role, Review
+
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+    db.create_all()
