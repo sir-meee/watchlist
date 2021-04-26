d33ca91281106d71b542bf001777cf91b358ef4a
sir-meee <sammymbevi@gmail.com>
2021-04-26 16:06:24 +0300

    database url

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class TestConfig(Config):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL?sslmode=require'")
 
 
 class DevConfig(Config):
